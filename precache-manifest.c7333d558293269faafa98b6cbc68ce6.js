e7e2803cc06e665089ab4c220e0f99fa0546fe79
AbdelrhmanAmin <maxhd213@gmail.com>
2021-08-23 05:42:27 +0200

    Updates

---
+++ precache-manifest.c7333d558293269faafa98b6cbc68ce6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9a12237de4be811d865d6ba2aa07c6",
+    "url": "/Nasa-Gallery/index.html"
+  },
+  {
+    "revision": "fb0e7f33bb4d9a271098",
+    "url": "/Nasa-Gallery/static/css/main.e6f5173d.chunk.css"
+  },
+  {
+    "revision": "8039e8dce4408a8e694c",
+    "url": "/Nasa-Gallery/static/js/2.5eaa59f7.chunk.js"
+  },
+  {
+    "revision": "56aad740629281b6e3a29dc2a6018a80",
+    "url": "/Nasa-Gallery/static/js/2.5eaa59f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb0e7f33bb4d9a271098",
+    "url": "/Nasa-Gallery/static/js/main.49b18b73.chunk.js"
+  },
+  {
+    "revision": "e8c2ab353c50b7f2e785",
+    "url": "/Nasa-Gallery/static/js/runtime-main.0325039c.js"
+  },
+  {
+    "revision": "4a66c7aac687c7ee0b27aefd48d74ef2",
+    "url": "/Nasa-Gallery/static/media/bg.4a66c7aa.jpg"
+  },
+  {
+    "revision": "20750692f98428624a3a1dfb3b9ff4c4",
+    "url": "/Nasa-Gallery/static/media/loading.20750692.gif"
+  },
+  {
+    "revision": "c68f9eaae5f0a2023b6e86b9316bc37a",
+    "url": "/Nasa-Gallery/static/media/swipe.c68f9eaa.gif"
+  }
+]);
